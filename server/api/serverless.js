c3c9b03abfbf2ddf7bb2fbd8fde730bfacb57828
U-C-S <uchanakyasrinivas@gmail.com>
2023-03-22 18:47:32 +0530

    minor fix

--- server/api/serverless.js
+++ server/api/serverless.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(init);
+app.use("/", init);
 
 app.get("/", async (request, reply) => {
   reply.send({ message: "Hello from ficition-logs api" });
